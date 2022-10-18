46230ce82ca0181c06a1721cfe5a643adfef5e86
toer-AMV <41640090+toer-AMV@users.noreply.github.com>
2022-10-19 02:09:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oZkPt6qT0zXufkSyhKcK4MS1gsfU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Iib56icPCQuat0V72Iv0MK1FYo0MKDCnqatQumuKZ3U',
+      useTemplateId: 'Iib56icPCQuat0V72Iv0MK1FYo0MKDCnqatQumuKZ3U', 'tkoG2xLNsDfa6k7gQdTrPWbhKK8rPRfYy8MoZuijV7s'
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-23',
       festivals: [
