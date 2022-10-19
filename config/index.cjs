8ccfa09b6e93d087fc46c1aafac9e4b4cf90a127
toer-AMV <41640090+toer-AMV@users.noreply.github.com>
2022-10-19 10:13:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '洋洋',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oZkPt6qT0zXufkSyhKcK4MS1gsfU',
+      id: 'oZkPt6rY78jKMqlk1_dFEFJL3jXE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'tkoG2xLNsDfa6k7gQdTrPWbhKK8rPRfYy8MoZuijV7s',
+      useTemplateId: 'A8b3a_7_QNXLwppbnmfLB0k3dfZGELCM7Hm1vVXJCRI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-23',
       festivals: [
